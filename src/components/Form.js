8591794eb859ae9900ef3fc23387803ebf562f41
Paul Lam <paulhklam1122@gmail.com>
2019-06-17 18:58:37 -0700

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Form = () => (
+  <h2>Form</h2>
+)
+
+export default Form
---
+++ src/components/Weather.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Weather = () => (
+  <h2>Weather</h2>
+)
+
+export default Weather
